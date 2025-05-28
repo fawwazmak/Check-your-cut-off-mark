74defc833416f015bf79a580608f97ccf7219f04
fawwazmak <fawwazmakinde47@gmail.com>
2025-05-28 17:57:19 +0100

    corrected paths to some file

--- script/index.js
+++ script/index.js
@@ -8,71 +8,71 @@ schoolForm.addEventListener("submit", (e) => {
 
     switch (selectedSchool) {
         case "UNILAG": 
-            window.location.href = "includedSchool/unilag.html";
+            window.location.href = "/includedSchool/unilag.html";
             break;
 
         case "LASU":
-            window.location.href = "includedSchool/lasu.html";
+            window.location.href = "/includedSchool/lasu.html";
             break;
 
         case "LASUSTECH":
-            window.location.href = "includedSchool/lasustech.html";
+            window.location.href = "/includedSchool/lasustech.html";
             break;
 
         case "OAU":
-            window.location.href = "includedSchool/oau.html";
+            window.location.href = "/includedSchool/oau.html";
             break;
 
         case "OOU":
-            window.location.href = "includedSchool/oou.html";
+            window.location.href = "/includedSchool/oou.html";
             break;
 
         case "UNN":
-            window.location.href = "includedSchool/unn.html";
+            window.location.href = "/includedSchool/unn.html";
             break;
 
         case "YCT":
-            window.location.href = "includedSchool/yct.html";
+            window.location.href = "/includedSchool/yct.html";
             break;
 
         case "gaposa":
-            window.location.href = "includedSchool/gaposa.html";
+            window.location.href = "/includedSchool/gaposa.html";
             break;
 
         case "UNILORIN":
-            window.location.href = "includedSchool/uniilorin.html";
+            window.location.href = "/includedSchool/uniilorin.html";
             break;
 
         case "UI":
-            window.location.href = "includedSchool/ui.html";
+            window.location.href = "/includedSchool/ui.html";
             break;
 
         case "ATBU":
-            window.location.href = "includedSchool/atbu.html";
+            window.location.href = "/includedSchool/atbu.html";
             break;
 
         case "ABU":
-            window.location.href = "includedSchool/abu.html";
+            window.location.href = "/includedSchool/abu.html";
             break;
 
         case "BUK":
-            window.location.href = "includedSchool/buk.html";
+            window.location.href = "/includedSchool/buk.html";
             break;
 
         case "FUNAAB":
-            window.location.href = "includedSchool/funaab.html";
+            window.location.href = "/includedSchool/funaab.html";
             break;
 
         case "FUTA":
-            window.location.href = "includedSchool/futa.html";
+            window.location.href = "/includedSchool/futa.html";
             break;
 
         case "FUTMIN":
-            window.location.href = "includedSchool/futmin.html";
+            window.location.href = "/includedSchool/futmin.html";
             break;
 
         case "UNIZIK":
-            window.location.href = "includedSchool/unizik.html";
+            window.location.href = "/includedSchool/unizik.html";
             break;
 
         default:
